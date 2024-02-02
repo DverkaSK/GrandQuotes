5b743fdc039dd31e4eda920d7bf6e9a248d96006
DverkaSK <DverkaSK@gmail.com>
2024-02-03 00:33:37 +0300

    Init commit

---
+++ src/main/java/ru/dverkask/grandquotes/GrandQuotes.java
@@ -0,0 +1,11 @@
+package ru.dverkask.grandquotes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GrandQuotes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+}
